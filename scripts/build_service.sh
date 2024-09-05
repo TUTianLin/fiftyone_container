1e96e30cb02c591511b330e7d603114ee04e1661
tulin <tulin@kingwaytek.com>
2024-09-05 15:56:31 +0800

    refactor(docker): mv dockerfile & scripts to root dir

--- scripts/build_service.sh
+++ scripts/build_service.sh
@@ -9,4 +9,4 @@ echo -e "${DEBUG}docker build image${NC}"
 docker build -t tulin/fiftyone .
 
 echo -e "${DEBUG}run the container${NC}"
-docker run -v ${PWD}:/root/fiftyone --network host --name fiftyone -it tulin/fiftyone
+docker run -v ${PWD}/fiftyone:/root/fiftyone --network host --name fiftyone -it tulin/fiftyone
